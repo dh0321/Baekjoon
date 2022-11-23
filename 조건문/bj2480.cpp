69996e95295d325aa9cd03d4e6907c0e220cc9b8
dh0321 <111106711+dh0321@users.noreply.github.com>
2022-11-23 09:50:10 +0900

    Create bj2480.cpp

---
+++ 조건문/bj2480.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int c;
+
+	int result;
+
+	cin >> a >> b >> c;
+
+	if (a == b && b == c && a == c)
+	{
+		result = 10000 + a * 1000;
+		cout << result << endl;
+	}
+	else if (a == b && a != c)
+	{
+		result = 1000 + a * 100;
+		cout << result << endl;
+	}
+	else if (a == c && a != b)
+	{
+		result = 1000 + a * 100;
+		cout << result << endl;
+	}
+	else if (b == c && a != b)
+	{
+		result = 1000 + b * 100;
+		cout << result << endl;
+	}
+	else
+	{
+		if (a > b && a > c)
+		{
+			result = a * 100;
+			cout << result << endl;
+		}
+		else if (b > a && b > c)
+		{
+			result = b * 100;
+			cout << result << endl;
+		}
+		else if (c > a && c > b)
+		{
+			result = c * 100;
+			cout << result << endl;
+		}
+	}
+
+	return 0;
+}
