dedd984cdcaf8bd93571d51dd99b3c19f5d83f33
dh0321 <111106711+dh0321@users.noreply.github.com>
2022-11-25 19:12:37 +0900

    Create bj11021.cpp

---
+++ 반복문/bj11021.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int t;
+    int a, b;
+    int result;
+
+    cin >> t;
+
+    for(int i = 0; i < t; i++)
+    {
+        cin >> a >> b;
+        result = a + b;
+        cout<<"Case #"<<i + 1<<": "<<result<<endl;
+    }
+
+
+    return 0;
+}
