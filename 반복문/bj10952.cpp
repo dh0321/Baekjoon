62cd9d59374347b04147bffb3480c6769bb1b652
dh0321 <111106711+dh0321@users.noreply.github.com>
2022-11-29 09:24:22 +0900

    Create bj10952.cpp

---
+++ 반복문/bj10952.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	int t = 10;
+
+	while (true)
+	{
+		cin >> a >> b;
+
+		if (a == 0 && b == 0)
+		{
+			break;
+		}
+
+		cout << a + b << endl;
+
+	}
+
+	return 0;
+}
