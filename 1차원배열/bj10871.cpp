0469c12edf4d2ae48a3b96e3ecf0187bd6c0cd8b
dh0321 <111106711+dh0321@users.noreply.github.com>
2022-11-30 23:49:30 +0900

    Update bj10871.cpp

--- 1차원배열/bj10871.cpp
+++ 1차원배열/bj10871.cpp
@@ -1,19 +1,25 @@
+#include <iostream>
+using namespace std;
+
 int main() {
- 
-	int N;
-	int X;
-	cin >> N >> X;	// N과 X 입력
- 
-	int arr[10000];	// 최대 가질 수 있는 N은 10000이므로, 10000칸의 배열 생성
- 
-	for(int i = 0; i < N; i++) {
-		cin >> arr[i];		// 배열에 N개의 수를 저장
+
+	int n, x;
+	int a[100];
+
+	cin >> n >> x;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
 	}
- 
-	for(int i = 0; i < N; i++) {
-		// i번째 원소가 X보다 작다면 출력
-		if(arr[i] < X) {
-			cout << arr[i] << " ";
+
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] < x)
+		{
+			cout << a[i] << " ";
 		}
 	}
+
+	return 0;
 }
