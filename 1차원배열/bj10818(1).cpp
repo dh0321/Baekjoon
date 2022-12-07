51e5b0006b2618273597cabf7b81bc5d0325275d
dh0321 <111106711+dh0321@users.noreply.github.com>
2022-12-07 18:31:56 +0900

    Update and rename bj10818.cpp to bj10818(1).cpp

---
+++ 1차원배열/bj10818(1).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	int array[n];
+ 	int min = 1000000;
+  	int max = -1000000;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> array[i];
+   	 if(max < array[i]) {
+ 		max = array[i];
+	    }
+	 if(min > array[i]) {
+	        min = array[i];
+    }
+	}
+
+	 cout << min << " " << max << endl;
+
+	return 0;
+
+}
+
