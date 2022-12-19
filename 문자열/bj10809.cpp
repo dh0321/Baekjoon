19d709353fd7c185e0e4ffee851a21414939301a
dh0321 <111106711+dh0321@users.noreply.github.com>
2022-12-19 23:47:27 +0900

    Create bj10809.cpp

---
+++ 문자열/bj10809.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	
+	string word;
+	int alpha[26];
+
+	cin >> word;
+
+	for (int i = 0; i < 26; i++) {
+		alpha[i] = -1; //모든 알파벳을 -1로 초기화
+	}
+
+	for (int i = 0; i < word.length(); i++) {
+
+    
+    
+    
+    
+	}
+
+	return 0;
+}
